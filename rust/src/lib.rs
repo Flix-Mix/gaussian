fa8b93d290d45eeced2157a78f4868fa67e34fcc
Aidan <agoldfa7@u.rochester.edu>
2021-04-02 17:45:20 -0400

    Refactored data passage

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,156 +1,182 @@
 use std::process;
 
-pub fn init(nsize: usize, matrix: &mut Vec<Vec<f32>>, b: &mut Vec<f32>, swap: &mut Vec<u32>, c: &mut Vec<f32>, v: &mut Vec<f32>){
-	for i in 0..nsize{
-		matrix.push(Vec::with_capacity(nsize));
-		for _ in 0..nsize{
-			matrix[i].push(0.0);
-		}
-		b.push(i as f32);
-		swap.push(i as u32);
-		c.push(0.0);
-		v.push(0.0);
-	}
+#[macro_use]
+macro_rules! SWAP {
+    ($a:expr,$b:expr) => {{
+        // $a ^= $b;
+        // $b ^= $a;
+        // $a ^= $b;
+        let t = $a;
+        $a = $b;
+        $b = t;
+    }};
+}
 
-	for i in 0..matrix.len(){
-		for j in 0..matrix.len(){
-			let ii: f32 = i as f32;
-			let jj: f32 = j as f32;
-			matrix[i][j] = if jj < ii {2.0*(jj+1.0)} else {2.0*(ii+1.0)};
-		}
-	}
+pub struct Data<'a> {
+    pub nsize: usize,
+    pub matrix: &'a mut Vec<Vec<f32>>,
+    pub b: &'a mut Vec<f32>,
+    pub c: &'a mut Vec<f32>,
+    pub v: &'a mut Vec<f32>,
+    pub swap: &'a mut Vec<u32>,
+}
+
+pub fn init(data: &mut Data) {
+    for i in 0..data.nsize {
+        data.matrix.push(Vec::with_capacity(data.nsize));
+        for _ in 0..data.nsize {
+            data.matrix[i].resize(data.nsize, 0.0);
+        }
+        data.b.push(i as f32);
+        data.swap.push(i as u32);
+        data.c.push(0.0);
+        data.v.push(0.0);
+    }
+
+    for i in 0..data.matrix.len() {
+        for j in 0..data.matrix.len() {
+            let ii: f32 = i as f32;
+            let jj: f32 = j as f32;
+            data.matrix[i][j] = if jj < ii {
+                2.0 * (jj + 1.0)
+            } else {
+                2.0 * (ii + 1.0)
+            };
+        }
+    }
 }
 
 //#[allow(dead_code,unused_variables)]
-pub fn compute_gauss(nsize: usize, matrix: &mut Vec<Vec<f32>>, b: &mut Vec<f32>, swap: &mut Vec<u32>){
-	for i in 0..nsize{
-		pivot(matrix,nsize,i,b,swap);
+pub fn compute_gauss(data: &mut Data) {
+    for i in 0..data.nsize {
+        pivot(data, i);
 
-		let mut pivot_val;// = matrix[i][i];
-		for j in i+1..nsize{
-			pivot_val = matrix[j][i];
-			matrix[j][i] = 0.0;
-			for k in i+1..nsize{
-				matrix[j][k] -= pivot_val * matrix[i][k];
-			}
-			b[j] -= pivot_val * b[i];
-		}
-	}
+        let mut pivot_val; // = matrix[i][i];
+        for j in i + 1..data.nsize {
+            pivot_val = data.matrix[j][i];
+            data.matrix[j][i] = 0.0;
+            for k in i + 1..data.nsize {
+                data.matrix[j][k] -= pivot_val * data.matrix[i][k];
+            }
+            data.b[j] -= pivot_val * data.b[i];
+        }
+    }
 }
 
 //#[allow(dead_code,unused_variables)]
-fn pivot(matrix: &mut Vec<Vec<f32>>, nsize: usize, currow: usize, b: &mut Vec<f32>, swap: &mut Vec<u32>){
-	let (mut irow, mut big, mut tmp);
+fn pivot(data: &mut Data, currow: usize) {
+    let (mut irow, mut big, mut tmp);
 
-	big = matrix[currow][currow];
-	irow = currow;
+    big = data.matrix[currow][currow];
+    irow = currow;
 
-	if big == 0.0 {
-		for i in currow..nsize{
-			tmp = matrix[i][currow];
-			if tmp != 0.0 {
-				big = tmp;
-				irow = i;
-				break;
-			}
-		}
-	}
+    if big == 0.0 {
+        //for i in currow..nsize {
+        for (i, row) in data.matrix.iter().enumerate().take(data.nsize).skip(currow) {
+            tmp = row[currow];
+            if tmp != 0.0 {
+                big = tmp;
+                irow = i;
+                break;
+            }
+        }
+    }
 
-	if big == 0.0 {
-		println!("The matrix is singular");
-		process::exit(-1);
-	}
+    if big == 0.0 {
+        println!("The matrix is singular");
+        process::exit(-1);
+    }
 
-	if irow != currow {
-		for i in currow..nsize {
-			let tmp = matrix[irow][i]; //make this a macro
-			matrix[irow][i] = matrix[currow][i];
-			matrix[currow][i] = tmp;
-		}
-		let tmp = b[irow];
-		b[irow] = b[currow];
-		b[currow] = tmp;
+    if irow != currow {
+        for i in currow..data.nsize {
+            SWAP!(data.matrix[irow][i], data.matrix[currow][i]);
+            // let tmp = matrix[irow][i]; //make this a macro
+            // matrix[irow][i] = matrix[currow][i];
+            // matrix[currow][i] = tmp;
+        }
+        data.b.swap(irow, currow);
 
-		let tmp = swap[irow];
-		swap[irow] = swap[currow];
-		swap[currow] = tmp;
-	}
+        data.swap.swap(irow, currow)
+    }
 
-	{
-		let pivot_val = matrix[currow][currow];
-		if pivot_val != 1.0{
-			matrix[currow][currow] = 1.0;
-			for i in currow+1..nsize{
-				matrix[currow][i] /= pivot_val;
-			}
-			b[currow] /= pivot_val;
-		}
-	}
+    {
+        let pivot_val = data.matrix[currow][currow];
+        if (pivot_val - 1.0).abs() > 0.0000001 {
+            data.matrix[currow][currow] = 1.0;
+            for i in currow + 1..data.nsize {
+                data.matrix[currow][i] /= pivot_val;
+            }
+            data.b[currow] /= pivot_val;
+        }
+    }
 }
 
-#[allow(dead_code,unused_variables)]
-fn solve_gauss(matrix: &mut Vec<Vec<f32>>, nsize: usize, b: &Vec<f32>, c: &mut Vec<f32>, v: &mut Vec<f32>){
-	v[nsize-1] = b[nsize-1];
-	for i in (0..nsize-2).rev(){
-		v[i] = b[i];
-		for j in (i+1..nsize-1).rev(){
-			v[i] -= matrix[i][j] * v[j];
-		}
-	}
-	for i in 0..nsize{
-		c[i] = v[i];
-	}
+#[allow(dead_code, unused_variables)]
+fn solve_gauss(data: Data) {
+    data.v[data.nsize - 1] = data.b[data.nsize - 1];
+    for i in (0..data.nsize - 2).rev() {
+        data.v[i] = data.b[i];
+        for j in (i + 1..data.nsize - 1).rev() {
+            data.v[i] -= data.matrix[i][j] * data.v[j];
+        }
+    }
+    // for i in 0..nsize {
+    //     c[i] = v[i];
+    // }
+    data.c[..data.nsize].clone_from_slice(&data.v[..data.nsize]);
 }
 
-
-pub fn print(matrix: &Vec<Vec<f32>>){
-	print!("{{\n");
-	for i in 0..matrix.len()-1{
-		println!(" {:?}",matrix[i]);
-	}
-	print!(" {:?}",matrix[matrix.len()-1]);
-	println!("\n}}");
+pub fn print(data: &Data) {
+    println!("{{");
+    for row in data.matrix.iter().take(data.matrix.len() - 1) {
+        println!(" {:?}", row);
+    }
+    print!(" {:?}", data.matrix[data.matrix.len() - 1]);
+    println!("\n}}");
 }
-
-
-// #[macro_export]
-// macro_rules! SWAP {
-// 	($a:expr,$b:expr) => {
-// 		{
-// 			let t = $a;
-// 			$a = $b;
-// 			$b = $t;
-// 		}
-// 	}
-// }
 
 #[cfg(test)]
-mod tests {	
-	use super::*;
+mod tests {
+    use super::*;
 
     #[test]
     fn init_matrix_test() {
-    	let nsize = 3;
+        let nsize = 3;
         let mut matrix: Vec<Vec<f32>> = Vec::with_capacity(nsize);
         let mut b: Vec<f32> = Vec::with_capacity(nsize);
         let mut c: Vec<f32> = Vec::with_capacity(nsize);
-		let mut v: Vec<f32> = Vec::with_capacity(nsize);
-		let mut swap: Vec<u32> = Vec::new();
-        init(nsize,&mut matrix,&mut b,&mut swap, &mut c, &mut v);
-        assert_eq!(matrix, [[2.0,2.0,2.0],[2.0,4.0,4.0],[2.0,4.0,6.0]]);
+        let mut v: Vec<f32> = Vec::with_capacity(nsize);
+        let mut swap: Vec<u32> = Vec::with_capacity(nsize);
+        let mut data = Data {
+            nsize: nsize,
+            matrix: &mut matrix,
+            b: &mut b,
+            c: &mut c,
+            v: &mut v,
+            swap: &mut swap,
+        };
+        init(&mut data);
+        assert_eq!(matrix, [[2.0, 2.0, 2.0], [2.0, 4.0, 4.0], [2.0, 4.0, 6.0]]);
     }
 
     #[test]
     fn compute_gauss_test() {
-    	let nsize = 3;
+        let nsize = 3;
         let mut matrix: Vec<Vec<f32>> = Vec::with_capacity(nsize);
         let mut b: Vec<f32> = Vec::with_capacity(nsize);
         let mut c: Vec<f32> = Vec::with_capacity(nsize);
-		let mut v: Vec<f32> = Vec::with_capacity(nsize);
-		let mut swap: Vec<u32> = Vec::with_capacity(nsize);
-        init(nsize,&mut matrix,&mut b,&mut swap, &mut c, &mut v);
-        compute_gauss(nsize,&mut matrix,&mut b,&mut swap);
-        assert_eq!(matrix, [[1.0,1.0,1.0],[0.0,1.0,1.0],[0.0,0.0,1.0]]);
+        let mut v: Vec<f32> = Vec::with_capacity(nsize);
+        let mut swap: Vec<u32> = Vec::with_capacity(nsize);
+        let mut data = Data {
+            nsize: nsize,
+            matrix: &mut matrix,
+            b: &mut b,
+            c: &mut c,
+            v: &mut v,
+            swap: &mut swap,
+        };
+        init(&mut data);
+        compute_gauss(&mut data);
+        assert_eq!(matrix, [[1.0, 1.0, 1.0], [0.0, 1.0, 1.0], [0.0, 0.0, 1.0]]);
     }
-} 
- 
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,20 +1,27 @@
 use std::env;
 pub mod lib;
 
-fn main(){
-	let args: Vec<String> = env::args().collect();
-	
-	let nsize: usize = args[1].parse::<usize>().unwrap();
-	
-	let mut matrix: Vec<Vec<f32>> = Vec::with_capacity(nsize);
-	let mut b: Vec<f32> = Vec::with_capacity(nsize);
-	let mut c: Vec<f32> = Vec::with_capacity(nsize);
-	let mut v: Vec<f32> = Vec::with_capacity(nsize);
-	let mut swap: Vec<u32> = Vec::with_capacity(nsize);
+fn main() {
+    let args: Vec<String> = env::args().collect();
 
+    let size: usize = args[1].parse::<usize>().unwrap();
+    let mut matrix: Vec<Vec<f32>> = Vec::with_capacity(size);
+    let mut b: Vec<f32> = Vec::with_capacity(size);
+    let mut c: Vec<f32> = Vec::with_capacity(size);
+    let mut v: Vec<f32> = Vec::with_capacity(size);
+    let mut swap: Vec<u32> = Vec::with_capacity(size);
 
-	lib::init(nsize,&mut matrix,&mut b,&mut swap, &mut c, &mut v);
-	lib::print(&matrix);
-	lib::compute_gauss(nsize,&mut matrix,&mut b,&mut swap);
-	lib::print(&matrix);
+    let mut data = lib::Data {
+        nsize: size,
+        matrix: &mut matrix,
+        b: &mut b,
+        c: &mut c,
+        v: &mut v,
+        swap: &mut swap,
+    };
+
+    lib::init(&mut data);
+    lib::print(&data);
+    lib::compute_gauss(&mut data);
+    lib::print(&data);
 }
